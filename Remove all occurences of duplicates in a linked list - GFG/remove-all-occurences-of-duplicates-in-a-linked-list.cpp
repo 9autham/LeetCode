147864afefa0255784d1611745ac0156bf67afe3
KURUVA GAUTHAM KUMAR <91959504+9autham@users.noreply.github.com>
2023-04-10 20:57:28 +0530

    Added solution - LeetHub

---
+++ Remove all occurences of duplicates in a linked list - GFG/remove-all-occurences-of-duplicates-in-a-linked-list.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+// } Driver Code Ends
+//User function Template for C++
+
+/* Linked List node structure
+    
+struct Node {
+	int data;
+	struct Node *next;
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* removeAllDuplicates(struct Node* head)
+    {
+        //code here
+        Node*dummy=new Node(-1);
+        dummy->next=head;
+        head=dummy;
+        Node*p=head;
+        int val=-1000;
+        Node*prev=head;
+        while(p->next){
+            if(p->data==p->next->data){
+                p->next=p->next->next;
+                val=p->data;
+            }
+            else if(val==p->data){
+                prev->next=p->next;
+                p=p->next;
+            }
+            else{
+                prev=p;
+                p=p->next;
+            }
+        }
+        if(val==p->data){
+            prev->next=NULL;
+        }
+        return head->next;
+    }
+};
+
+//{ Driver Code Starts.
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+// Driver program to test above functions
+int main()
+{
+	int T;
+	cin >> T;
+
+
+	while (T--)
+	{
+		int N;
+
+		cin >> N ;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < N; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+		Solution ob;
+		head = ob.removeAllDuplicates(head);
+		printList(head);
+
+		cout << "\n";
+
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends

658ef79ad404bc5ef8f8ca1d2fec4604bb9345ac
KURUVA GAUTHAM KUMAR <91959504+9autham@users.noreply.github.com>
2023-05-12 22:31:06 +0530

    Added solution - LeetHub

---
+++ Minimum indexed character - GFG/minimum-indexed-character.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find the minimum indexed character.
+    int minIndexChar(string str, string patt)
+    {
+        // Your code here
+set<char>s;
+       for(auto value:patt){
+           s.insert(value);
+       }
+       for(int i=0;i<str.size();i++){
+           if(s.find(str[i])!=s.end()){
+               return i;
+           }
+   
+       }
+       return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+    cin>>t;
+    while(t--)
+    {
+        string str;
+        string patt;
+        cin>>str;
+        cin>>patt;
+        Solution obj;
+        cout<<obj.minIndexChar(str, patt)<<endl;
+    }
+	return 0;
+}
+  // } Driver Code Ends

b08871d42e71dee78aeb6b561d13fdd408213996
KURUVA GAUTHAM KUMAR <91959504+9autham@users.noreply.github.com>
2023-07-19 22:28:36 +0530

    Added solution - LeetHub

--- Overlapping Intervals - GFG/overlapping-intervals.cpp
+++ Overlapping Intervals - GFG/overlapping-intervals.cpp
@@ -5,32 +5,31 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    vector<vector<int>> overlappedInterval(vector<vector<int>>& in) {
-         // Code here
-         sort(in.begin(),in.end());
-         int start=in[0][0];
-         int end=in[0][1];
-         int n=in.size();
-        //  in.push_back({INT_MAX,INT_MAX});
+    vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
          vector<vector<int>>ans;
-         for(int i=1;i<n;i++){
-             if(in[i][0]>end){
-                 vector<int>v(2);
-                 v[0]=start;
-                 v[1]=end;
-                 ans.push_back(v);
-                 start=in[i][0];
-                 end=in[i][1];
-             }
-             else{
-                 end=max(end,in[i][1]);
-             }
-         }
-         vector<int>v(2);
-                 v[0]=start;
-                 v[1]=end;
-                 ans.push_back(v);
-         return ans;
+        sort(intervals.begin(),intervals.end());
+        int n=intervals.size();
+        int x=intervals[0][0];
+        int y=intervals[0][1];
+        for(int i=1;i<n;i++){
+            if((x<=intervals[i][0] and y>=intervals[i][0]) or (x<=intervals[i][0] and y>=intervals[i][1])){
+                x=min(x,intervals[i][0]);
+                y=max(y,intervals[i][1]);
+            }
+            else{
+                vector<int>v(2);
+                v[0]=x;
+                v[1]=y;
+                ans.push_back(v);
+                x=intervals[i][0];
+                y=intervals[i][1];
+            }
+        }
+        vector<int>v(2);
+                v[0]=x;
+                v[1]=y;
+                ans.push_back(v);
+                return ans;
     }
 };
 
